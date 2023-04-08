1bbed4a04aa88751de77a02e13d0d52bb9e1d86a
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-07 20:59:17 -0500

    attempting to insert a clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentTime = dayjs("2023-04-04");
+var currentDate = dayjs("calendar");
 
-$("#currentTime").text(currentTime.format("MMM DD, YYYY"));
+$("#currentTime").text(currentDate.format("MMM DD, YYYY"));
 
+var saveEl = $("#fa-save");
+
+function currentTime() {
+  var date = new Date(); 
+  var hh = date.getHours();
+  var mm = date.getMinutes();
+  var ss = date.getSeconds();
+  var session = "AM";
+
+    
+  if(hh > 12){
+      session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time; 
+  var t = setTimeout(function(){ currentTime() }, 1000); 
+
+}
+
+currentTime();
 
 $(function () {
   El.addEventListener()
