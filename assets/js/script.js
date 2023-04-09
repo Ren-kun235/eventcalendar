a05583f21f24e5298862fa0731abc38597599c2d
Ren-Kun235 <changpobzeb651@gmail.com>
2023-04-08 19:49:27 -0500

    added the rest of the timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var currentDate = dayjs("calendar");
+var currentDate = dayjs("2023-04-08");
 
 $("#currentTime").text(currentDate.format("MMM DD, YYYY"));
 
